76274f8b978bb385736799c7141d7f068366dc04
DioneKx <mjhonnyvieira@gmail.com>
2024-03-24 18:54:45 -0300

    Jhonny: Intemediary-Upgrade-Incomplete

--- AppProject/src/components/TaskAction/Intermediary/index.js
+++ AppProject/src/components/TaskAction/Intermediary/index.js
@@ -1,7 +1,8 @@
-import { Text, View, TouchableOpacity } from "react-native";
 import * as React from "react";
 import api from "../../../../api";
 import { styles } from "./style";
+import { Ionicons, AntDesign } from '@expo/vector-icons';
+import { Text, View, TouchableOpacity, ScrollView } from "react-native";
 
 export function IntermediaryTask(props) {
     const dataT = props.data;
@@ -11,7 +12,9 @@ export function IntermediaryTask(props) {
     const [taskT, setTaskT] = React.useState(dataT.task_text);
     const [answer, setAnswer] = React.useState([]);
     const [alt, setAlt] = React.useState();
+    const [altBkp, setAltBkp] = React.useState();
     const [visible, setVisible] = React.useState(false);
+    const [listAlt, setListAlt] = React.useState([]);
 
     const getAnswer = async () => {
         try {
@@ -61,6 +64,7 @@ export function IntermediaryTask(props) {
         }
 
         setAlt(splitT);
+        setAltBkp(splitT);
         setVisible(true);
     };
 
@@ -77,13 +81,21 @@ export function IntermediaryTask(props) {
         } else {
             alert("Errouuuuuu!");
             setTaskT(dataT.task_text);
+            setAlt(altBkp);
+            setListAlt([]);
         }
     }
 
     async function replaceTxt(e) {
+        const altIndex = alt.findIndex((value, index, array) => { return value == e })
+        const remove = alt.toSpliced(altIndex, 1)
+        const list = listAlt
+        setAlt(remove);
+
         const answerC = answer.answer_text.split(" ");
         const alternate = taskT.replace("‼", e);
-        let temp = "";
+        let temp = `${e}`; list.push(e)
+        console.log(list)
 
         if (count == 0) {
             setAnswerU(`${e}`);
@@ -94,14 +106,22 @@ export function IntermediaryTask(props) {
 
         setTaskT(`${alternate}`);
         setCount(count + 1);
+        setListAlt(list)
 
         if (count >= answerC.length - 1) {
             setTimeout(() => { altCompare(temp) }, 250);
         }
     }
 
+    function reloadT() {
+        setCount(0);
+        setAlt(altBkp);
+        setTaskT(dataT.task_text);
+        setListAlt([])
+    }
+
     if (visible == true) {
-        const listAlts = alt.map((e) => {
+        const listAlts = () => alt.map((e) => {
             return (
                 <TouchableOpacity style={styles.button} onPress={() => replaceTxt(e)}>
                     <Text style={styles.title}>{e}</Text>
@@ -109,12 +129,45 @@ export function IntermediaryTask(props) {
             );
         });
 
+        const altRemoved = () => listAlt.map((e) => {
+            try {
+                return (
+                    <TouchableOpacity style={styles.button} onPress={() => replaceTxt(e)}>
+                        <Text style={styles.title}>{e}</Text>
+                    </TouchableOpacity>
+                );
+            } catch { [] }
+        })
+
         return (
             <View style={styles.container}>
-                <View style={styles.contentA}>
-                    <><Text style={styles.titleB}>{taskT}</Text></>
+                <View style={styles.content}>
+                    <View style={styles.contentBtn}>
+                        <TouchableOpacity style={styles.btn}>
+                            <AntDesign
+                                name="questioncircleo"
+                                size={24}
+                                color="#06c244"
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.btn} onPress={() => reloadT()}>
+                            <Ionicons
+                                size={24}
+                                color="#06c244"
+                                name="reload"
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.contentText}>
+                        <Text style={styles.titleB}>{taskT}</Text>
+                    </View>
                 </View>
-                <View style={styles.content}>{listAlts}</View>
+                <View style={styles.contentA}>
+                    {altRemoved()}
+                </View>
+                <View style={styles.contentB}>
+                    <ScrollView contentContainerStyle={styles.contentScroll}>{listAlts()}</ScrollView>
+                </View>
             </View>
         );
     } else {
